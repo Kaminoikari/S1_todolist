2da81ec58d958032ac3733100c01c1ce90897bd2
Charles <charles0527@hotmail.com>
2022-04-01 03:29:45 +0800

    feat: add relations between user and todo models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -8,49 +8,53 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const name = req.body.name
-
-  return Todo.create({ name })
+  return Todo.create({ name, userId })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
 router.get('/:id', (req, res) => {
+  const userId = req.user._id;
   const id = req.params.id
-  return Todo.findById(id)
+  return Todo.findByOne({ _id, userId })
     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
+  const userId = req.user._id;
   const id = req.params.id
-  return Todo.findById(id)
+  return Todo.findByOne({ _id, userId })
     .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.log(error))
+    .then((todo) => res.render('edit', { todo }))
+    .catch((error) => console.log(error));
 })
 
 router.put('/:id', (req, res) => {
+  const userId = req.user._id;
   const id = req.params.id
   const { name, isDone } = req.body
 
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
+  return Todo.findByOne({ _id, userId })
+    .then((todo) => {
+      todo.name = name;
+      todo.isDone = isDone === 'on';
+      return todo.save();
     })
     .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 })
 
 router.delete('/:id', (req, res) => {
+  const userId = req.user._id;
   const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
+  return Todo.findByOne({ _id, userId })
+    .then((todo) => todo.remove())
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 })
 
 module.exports = router
