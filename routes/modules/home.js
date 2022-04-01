214d27a3007278677befa1750dc5eff16eb2d73d
Charles <charles0527@hotmail.com>
2022-04-02 02:01:16 +0800

    fix: todo-list routing

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const Todo = require('../../models/todo')
+const Todo = require('../../models/todo');
 
 router.get('/', (req, res) => {
   const userId = req.user._id;
-  Todo.find( userId )
+  Todo.find({ userId })
     .lean()
-    .sort({ _id: 'asc' }) // desc
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.error(error))
-})
+    .sort({ _id: 'asc' })
+    .then((todos) => res.render('index', { todos }))
+    .catch((error) => console.error(error));
+});
 
-module.exports = router
+module.exports = router;
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/user');
+
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
+
+const User = require('../../models/user');
 
 router.get('/login', (req, res) => {
   res.render('login');
@@ -12,7 +14,7 @@ router.post(
   '/login',
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/users/login',
+    failureRedirect: 'users/login',
   })
 );
 
@@ -21,57 +23,52 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-  // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body;
-  // 以上為以下語法的結構複製
-  // const name = req.body.name
-  // const email = req.body.email;
-  // const password = req.body.password;
-  // const confirmPassword = req.body.confirmPassword;
-  const errors = []
+  const errors = [];
+
   if (!name || !email || !password || !confirmPassword) {
-    errors.push ({message: '所有欄位都是必填。'})
+    errors.push({ message: '所有欄位都是必填。' });
   }
-
-  if (password !== confirmPassword ) {
-    errors.push ({message: '密碼與確認密碼不相符！'})
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' });
   }
-
   if (errors.length) {
     return res.render('register', {
       errors,
       name,
       email,
       password,
-      confirmPassword
-    })
+      confirmPassword,
+    });
   }
 
-    // 檢查使用者是否已經註冊
-    User.findOne({ email })
-      .then((user) => {
-        // 如果已經註冊：退回原本畫面
-        if (user) {
-          errors.push({ message: '這個 Email 已經註冊過了。' });
-          res.render('register', {
-            errors,
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        errors.push({ message: '這個 Email 已經註冊過了。' });
+        return res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword,
+        });
+      }
+
+      return bcrypt
+        .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+        .then((salt) => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+        .then((hash) =>
+          User.create({
             name,
             email,
-            password,
-            confirmPassword,
-          });
-        } return bcrypt
-      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
-      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash // 用雜湊值取代原本的使用者密碼
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-      .catch((err) => console.log(err));
+            password: hash, // 用雜湊值取得原本的使用者密碼
+          })
+        )
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 });
 
 router.get('/logout', (req, res) => {
