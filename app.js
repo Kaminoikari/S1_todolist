078784fcea21d239293dffbce0a8c212ce6ed572
Charles <charles0527@hotmail.com>
2022-04-04 01:14:12 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const flash = require ('connect-flash')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const routes = require('./routes');
 require('./config/mongoose');
 
